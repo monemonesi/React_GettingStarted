865ed4017d477e118d517ad4d3385b1bb91184d7
Roberto Monesi <roberto.monesi@outlook.com>
2019-08-25 11:01:52 +0100

    bind the handleIncrement to the state of the component

--- basket-app/src/components/counter.jsx
+++ basket-app/src/components/counter.jsx
@@ -2,13 +2,37 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 }); //setState tell react that there have been an update!
+  };
+
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {this.renderTags()}
       </div>
     );
   }
