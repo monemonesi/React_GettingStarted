b34a20eeae8a47a7dbbcad6ede7b2948ad32d48a
Roberto Monesi <roberto.monesi@outlook.com>
2019-08-27 06:27:51 +0100

    Add multiple comoponents

--- basket-app/src/components/counter.jsx
+++ basket-app/src/components/counter.jsx
@@ -2,24 +2,12 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.value
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
-
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 }); //setState tell react that there have been an update!
+    //console.log(product);
+    this.setState({ value: this.state.value + 1 }); //setState tell react that there have been an update!
   };
 
   render() {
@@ -27,24 +15,23 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement()}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        {this.renderTags()}
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-"; //hold all the common classes description
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
